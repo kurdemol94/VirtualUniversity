f1f2d55575fe6c8485d1f3bf0ebfdde253a62ae0
kurdemol94 <kurdemol94@gmail.com>
2018-03-13 17:57:34 +0100

    first commit

---
+++ VirtualUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualUniversity.Models
+{
+    public class Student
+    {
+        //entity framework interprets ID as default primary key (has to be named ID or classnameID)
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
