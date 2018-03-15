6ab7296f9adef7ca363939760b91e20e96ea744d
kurdemol94 <kurdemol94@gmail.com>
2018-03-15 22:13:47 +0100

    controller and views for Student, scaffolded and tuned up

--- VirtualUniversity/Models/Student.cs
+++ VirtualUniversity/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,8 @@ namespace VirtualUniversity.Models
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
